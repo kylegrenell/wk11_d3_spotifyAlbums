fef1d8ab01ed1046d9c681af8fbffbaa22d793f2
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-12-09 18:48:02 +0000

    removed node modules

---
+++ public/app.js
@@ -0,0 +1,42 @@
+var app = function(){
+
+  var url = 'https://api.spotify.com/v1/search?q=christmas&type=album';
+  makeRequest(url, requestComplete);
+
+}
+
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = callback;
+  request.send();
+}
+
+var requestComplete = function(){
+  console.log("success");
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var spotify = JSON.parse(jsonString);
+  var albums = spotify.albums.items;
+  console.log(albums);
+  // once this information has been passed down call populate
+  populateList(albums);
+}
+
+var populateList = function(albums){
+
+  var ul = document.getElementById('albums');
+
+    albums.forEach(function(album){
+    var li = document.createElement('li');
+    li.innerText = "Artist: " + album.artists[0].name + "\nAlbum: " + album.name;
+    ul.appendChild(li);
+
+    // var li = document.createElement('li');
+    // li.innerText = ;
+    // ul.appendChild(li);
+    })
+
+}
+
+window.onload = app;
